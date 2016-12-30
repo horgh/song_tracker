edcf3c790364b9b3b306d64cc77c418494b2597f
Will Storey <will@summercat.com>
2016-12-30 15:47:46 -0800

    api: Improve comments
    A small cleanup commit. Improve comments and output. Switch to exit
    early in retrieving last play if user not found.

--- website/api.php
+++ website/api.php
@@ -1,14 +1,14 @@
 <?php
 /*
- * Provide api functions:
+ * Provide API functions:
  *  - new play: POST: user, pass, artist, album, title, length
  *  - last play: GET: user, last
  *
- * NOTE: response is not html encoded right now - it's just plaintext.
+ * NOTE: Response is plaintext UTF-8. No HTML encoding or anything.
  *
  * TODO:
- * - respond with json instead
- * - require an action parameter to identify the api request rather
+ * - Respond with JSON
+ * - Require an action parameter to identify the API request rather
  *   than just looking for the required arguments to identify it.
  */
 
@@ -16,44 +16,48 @@ require_once(__DIR__ . '/config/config.php');
 require_once(__DIR__ . '/src/model.User.php');
 require_once(__DIR__ . '/src/API.php');
 
-header('Content-type: text/plain; charset=utf-8');
+header('Content-Type: text/plain; charset=utf-8');
 
-// a play was sent.
-if (isset($_POST['artist']) && isset($_POST['album'])
-  && isset($_POST['title']) && isset($_POST['length'])
-  && isset($_POST['pass']) && isset($_POST['user']))
+// Play sent.
+if (isset($_POST['artist']) && isset($_POST['album']) &&
+  isset($_POST['title']) && isset($_POST['length']) &&
+  isset($_POST['pass']) && isset($_POST['user']))
 {
 
   if (!API::add_user_play($_POST['user'], $_POST['pass'], $_POST['artist'],
     $_POST['album'], $_POST['title'], $_POST['length']))
   {
     print "Error recording the play.\n";
-    print "api.php: Artist: " . $_POST['artist']
+    print "Artist: " . $_POST['artist']
       . " album: " . $_POST['album'] . " title: " . $_POST['title']
       . " length " . $_POST['length'] . "\n";
     exit;
   }
+
   print "Play recorded.\n";
   exit;
 }
 
-// Last song played in plain text for script (for now playing)
+// Retrieve last song played.
 if (isset($_GET['last']) && isset($_GET['user'])) {
   $user = new User();
-  if ($user->query_by_name($_GET['user'])) {
-    $lastSongPlay = $user->get_latest_songs(1);
-    if (count($lastSongPlay) === 1) {
-      $song = $lastSongPlay[0];
-      print $song->artist . " - " . $song->album . " - " . $song->title
-            . " (" . $song->length . ")\n";
-      exit;
-    }
-    print "Error fetching song.\n";
+
+  if (!$user->query_by_name($_GET['user'])) {
+    print "Invalid user.\n";
     exit;
   }
-  print "Invalid user.\n";
+
+  $lastSongPlay = $user->get_latest_songs(1);
+
+  if (count($lastSongPlay) === 1) {
+    $song = $lastSongPlay[0];
+    print $song->artist . " - " . $song->album . " - " . $song->title
+          . " (" . $song->length . ")\n";
+    exit;
+  }
+
+  print "Error fetching song.\n";
   exit;
 }
 
-// unknown api request.
 print "Unknown request.\n";
